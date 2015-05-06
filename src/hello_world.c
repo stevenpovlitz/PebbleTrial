fd46824f765489292c78113596d92fcba82009f7
Steven <steven.povlitz@gmail.com>
2015-05-06 13:50:53 -0700

    Pretty and Working
    well, not THAT pretty, but pretty good all the same. and i can close out the app without it crashing.

--- src/hello_world.c
+++ src/hello_world.c
@@ -2,6 +2,8 @@
 
 Window *window; // the pointer for the Window element
 TextLayer *text_layer;
+InverterLayer *inv_layer;
+
 char buffer[] = "00:00";
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed){ //updates watchface on minute
@@ -13,32 +15,34 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed){ //updates watc
 }
 
 void window_load(Window *window){
-  //to be added to
-  text_layer = text_layer_create(GRect(0, 0, 144, 168)); //0,0 origin, 
-  // layer of size 144 by 168 (aka full screen)
-  
-  text_layer_set_background_color(text_layer, GColorClear); // sets the layer’s 
-  // background colour using the supplied pointer and GColor type
-  text_layer_set_text_color(text_layer, GColorBlack); //sets the text colour itself
-  // in a similar manner to the set_background_color line
- 
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-  // adds the TextLayer to the Window as a child (shown in front)
-  
-  text_layer_set_text(text_layer, "white horse");
-  
-  //Get a time structure so that the face doesn't start blank
-  struct tm *t;
-  time_t temp;
-  temp = time(NULL);
-  t = localtime(&temp);
-  
-  //Manually call the tick handler when the window is loading
-  tick_handler(t, MINUTE_UNIT);
+  //Time layer
+	text_layer = text_layer_create(GRect(0, 53, 132, 168));
+	text_layer_set_background_color(text_layer, GColorClear);
+	text_layer_set_text_color(text_layer, GColorBlack);
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+	
+	layer_add_child(window_get_root_layer(window), (Layer*) text_layer);
+	
+	//Inverter layer
+	inv_layer = inverter_layer_create(GRect(0, 50, 144, 62));
+	layer_add_child(window_get_root_layer(window), (Layer*) inv_layer);
+	
+	//Get a time structure so that the face doesn't start blank
+	struct tm *t;
+	time_t temp;	
+	temp = time(NULL);	
+	t = localtime(&temp);	
+	
+	//Manually call the tick handler when the window is loading
+	tick_handler(t, MINUTE_UNIT); 
 }
 
 void window_unload(Window *window){
-  //to be added to
+  //We will safely destroy the Window's elements here!
+  text_layer_destroy(text_layer);
+ 
+  inverter_layer_destroy(inv_layer);
 }
 
 void init() { // set up app
@@ -54,10 +58,8 @@ void init() { // set up app
 
 void deinit() { // Frees up memory once app is done running
   // De-initialize the app elements here!
-  text_layer_destroy(text_layer); // because text_layer is a child of window, it must
-  // be destroyed first
-  window_destroy(window);
   tick_timer_service_unsubscribe();
+  window_destroy(window);
 }
 
 int main(void) {

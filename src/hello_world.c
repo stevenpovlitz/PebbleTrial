06a508f61129b369bf7a83cd86cca5a4182f3b73
Steven <steven.povlitz@gmail.com>
2015-05-09 23:22:59 -0700

    Correct height thrown output to logs
    FINALLY FINALLY FINALLY. I had to revisit 1st semester physics and do an UGLY workaround for printf not using floats correctly, but hey, the code does what I need it to. Ugly but functional.

--- src/hello_world.c
+++ src/hello_world.c
@@ -7,57 +7,60 @@
 #include <pebble.h>
 #include <stdlib.h> // for abs() method
 bool unsubbed = false;
-#define NUMSAMPLES 200
+#define NUMSAMPLES 200 // how many samples of accel data to capture
 
 static Window *s_main_window;
 static TextLayer *s_output_layer;
 // should this be declared in main, to save memory?
 int gatherStats = 0;
 int accelSamples [NUMSAMPLES][4]; //fourth is for absolute sum of three places
-// long accelTimeStamp [100]; //is a long actually big enough to hold these? I dunno, seems to work now
 
 // find num samples between max thrown height and impact with ground
 static int find_throw_length(){
   int throwLength = 0;
-  int h = 10; // not 0 to avoid initial vibs throwing off data
+
   
   // find a large val (prob start of throw) then find when ensuing dip aka throw
-  // ends with a dramatic change in abs acceleration
-  while (h < NUMSAMPLES){
+  // ends with a dramatic change in absolute acceleration
+  for(int h = 10; h < NUMSAMPLES; h++){ // h not 0 to avoid initial vibs throwing off data
     if (accelSamples[h][3] > 4000){ // high accel values = start of throw
       int biggest = accelSamples[h][3];
-      printf("Biggest aka over 4000: %d", biggest);
       int i = 0;
       for(int j = 0; j < 10; j++){
         if (accelSamples[h+j][3] >= biggest){
           i = h+j; // i is highest of values up to 10 after an over 4000 value
+          h = h+j; // use j to store start of throw
           biggest = accelSamples[h+j][3];
         }
       }
-      for(i = i+5; i < NUMSAMPLES - 2; i++){ // start looking for end of throw
-        if((accelSamples[i][3] - accelSamples[i+1][3]) > 2000){ //dramatic change in accel
+      for(i = i + 5; i < NUMSAMPLES - 2; i++){ // start looking for end of throw
+        if((accelSamples[i+1][3] - accelSamples[i][3]) > 2000){ //dramatic change in accel
           printf("i:%d h:%d" , i, h); // log out the data we want to work with
-          throwLength = i - h; // throw end - throw start
+          throwLength = i - h + 1; // throw end - throw start + 1 (because inclusive)
+          printf("Throw Start: %d\tThrow End: %d", biggest, accelSamples[throwLength][3]);
           return throwLength;
         }
       } 
     }
-    h++;
   }
   return -1;;
 }
 
 // output how high the pebble was thrown
 static void data_process(){ 
-  int x = find_throw_length(); // get length of throw in # of samples
+  float x = find_throw_length(); // get length of throw in # of samples
   if (x == -1) {
     printf ("No peak found, try tossing harder next time.");
   }
   else {
-    printf("Samples in throw: %d", x);
-    // should calc and store and output as a float
-    double heightThrown = ((double)x) / 2.0 / 25.0 * 9.8; // find in meters height thrown
-    printf ("Pebble was tossed %4d meters high.\n" , (int)(heightThrown * 100));
+    printf("Samples in throw: %d", (int)x);
+    // should calc and store and output
+    x = x / 2.0 / 25; // convers numsamples to seconds in decimal form
+    float heightThrown = .5 * 9.8 * (x * x); // find in meters height thrown
+    // ugly workaround because printf isn't working with float type
+    int aboveDec = (int)heightThrown;
+    int belowDec = (int)(heightThrown * 100) - ((int)heightThrown) * 100;
+    printf("Pebble was tossed %d.%d meters high.\n" , aboveDec, belowDec);
   }
 }
 static void data_handler(AccelData *data, uint32_t num_samples) {
@@ -70,14 +73,12 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
     accelSamples[gatherStats][1] = data[0].y;
     accelSamples[gatherStats][2] = data[0].z; 
     accelSamples[gatherStats][3] = abs(data[0].x) + abs(data[0].y) + abs(data[0].z);
-    // accelTimeStamp [gatherStats] = data[0].timestamp;
     
-    //data[0].timestamp is a "uint64_t", which I just cast to a long for convenience
-    printf("Index,X,Y,Z\t %5d\t %5d\t %5d\t %5d\t %5d",  //,abs:\t%10li taken out
-           gatherStats, data[0].x, data[0].y, data[0].z,accelSamples[gatherStats][3]); //(long)data[0].timestamp,  taken out
+    // print out xyz data to program logs. because of "\t", easily imported into excel
+    printf("Index,X,Y,Z\t %5d\t %5d\t %5d\t %5d\t %5d",
+           gatherStats, data[0].x, data[0].y, data[0].z,accelSamples[gatherStats][3]); 
     
-    //printf("Index, X, Y, X\t%5d\t%5d" , gatherStats, data[0].x);
-    
+    // show xyz data on pebble screen
     snprintf(s_buffer, sizeof(s_buffer), 
       "X,Y,Z\n> %d,%d,%d", 
       data[0].x, data[0].y, data[0].z
@@ -88,15 +89,14 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
     gatherStats++; // increment index counter
   }
   else {
-    // maybe later I can add some text to print out to the user (ie watch screen)
+    // After 200th accel data struct comes in, stop listening for accel data
     accel_data_service_unsubscribe();
     unsubbed = true;
-    
-    // unsubscribe from accel data, saves battery here
     printf("Done collecting data");
+    
     // vibrate to let user know data collection is done
     vibes_long_pulse();
-    //implements calculation function, uses the data
+    // use the data to calculate height thrown
     data_process();
   }
 }
